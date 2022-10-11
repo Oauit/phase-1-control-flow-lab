ca619a9ffd8b09b379f8f5916d493f052b4801a2
oauit <aidanjade.ward@gmail.com>
2022-10-11 15:46:51 -0600

    commit tree 0f6394ecb6324b7396f0f26caf572ddf5cf390bc

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+  console.log(199);
+  return 'This one is on me!'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+  else if(feet <= 2000){
+  console.log(1500);
+  return 'That will be twenty bucks.'
+} 
+
+else if(feet <= 2500){
+  console.log(2001);
+  return 'I will gladly take your thirty bucks.'
+} 
+
+else if(feet > 2500){
+  console.log(2501);
+  return 'No can do.'
+}
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+function ternaryCheckCity(city){
+  if (city === 'NYC'){
+    return 'Ok, sounds good.'
+  } else (false) 
+    return 'No go.'
+  }
+  
+
+
+
+
+  function switchOnCharmFromTip(tip){
+    switch(tip) {
+      case 'generous':
+        return 'Thank you so much.';
+        break;
+      case 'not as generous':
+        return 'Thank you.';
+        break;
+      case 'thanks for everything':
+        return 'Bye.'
+        break;
+    }
+
+  //   if (tip === 'generous'){
+//     return 'Thank you so much.'
+//   }
+//   else if(tip === 'not as generous'){
+//     return 'Thank you.'
+//   }
+//   else if(tip === 'thanks for everything'){
+//     return 'Bye.'
+//   }
+ }
